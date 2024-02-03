34ba968e4aa7fe3e8d80df98d38a97de99d67f5c
MrFixMan007 <schurkin.danil@yandex.ru>
2024-02-04 02:36:22 +0400

    добавлены фрагменты, навигация по фрагментам, анимации переходов, экшн бар наверху и мелкие исправления впридачу

--- app/src/main/java/com/example/engineersmetalcalcs/MainActivity.kt
+++ app/src/main/java/com/example/engineersmetalcalcs/MainActivity.kt
@@ -1,46 +1,19 @@
 package com.example.engineersmetalcalcs
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.engineersmetalcalcs.ui.theme.EngineersMetalCalcsTheme
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            EngineersMetalCalcsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
+        setupActionBarWithNavController(findNavController(R.id.fragmentContainerView))
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    EngineersMetalCalcsTheme {
-        Greeting("Android")
+    override fun onSupportNavigateUp(): Boolean {
+        val controller = findNavController(R.id.fragmentContainerView)
+        return controller.navigateUp() || super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/com/example/engineersmetalcalcs/fragments/LikvidTempMenuFragment.kt
+++ app/src/main/java/com/example/engineersmetalcalcs/fragments/LikvidTempMenuFragment.kt
@@ -5,11 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
 import com.example.engineersmetalcalcs.R
 
-class MainFragment : Fragment() {
+class LikvidTempMenuFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,14 +18,10 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        return inflater.inflate(R.layout.fragment_likvid_temp_menu, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val controller = findNavController()
-        val button1 = view.findViewById<Button>(R.id.main_menu_button1)
-
-        button1.setOnClickListener {controller.navigate(R.id.action_mainFragment_to_likvidTempMenuFragment)}
     }
 }
