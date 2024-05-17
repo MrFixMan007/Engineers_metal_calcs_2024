8195a6cd25d057f3e51b7db1079b0754c32763ac
MrFixMan007 <schurkin.danil@yandex.ru>
2024-05-17 15:07:53 +0400

    Модулю сохранений добавлены зависимости

--- feature-listing-all-saves/build.gradle.kts
+++ feature-listing-all-saves/build.gradle.kts
@@ -34,10 +34,51 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.12.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.activity:activity-compose:1.9.0")
+
+    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("androidx.compose.ui:ui")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    testImplementation ("androidx.test:core-ktx:1.5.0")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.6.7")
+
+    // navigation component implementation <
+    val navVersion = "2.7.7"
+    // Kotlin
+    implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
+    implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
+    // Feature module Support
+    implementation("androidx.navigation:navigation-dynamic-features-fragment:$navVersion")
+    // Testing Navigation
+    androidTestImplementation("androidx.navigation:navigation-testing:$navVersion")
+    // Jetpack Compose Integration
+    implementation("androidx.navigation:navigation-compose:$navVersion")
+    // navigation component implementation >
+
+    // Koin <
+    val koinVersion = "3.5.0"
+    val koinAndroidVersion = "3.5.0"
+    implementation(platform("io.insert-koin:koin-bom:$koinVersion"))
+    implementation("io.insert-koin:koin-core")
+    implementation("io.insert-koin:koin-android:$koinAndroidVersion")
+    testImplementation("io.insert-koin:koin-test:$koinVersion")
+    // Koin >
+
+    implementation(project(mapOf("path" to ":feature-listing-all-calcs:api")))
+    implementation(project(mapOf("path" to ":feature-calc-cargo-weight")))
+    implementation(project(mapOf("path" to ":core-ui")))
 }
